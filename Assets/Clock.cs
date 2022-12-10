3dd11ff8e4e1af2b182f9b1ca5f04ae8d439dcf2
openbye1164 <2833151405@qq.com>
2022-12-10 21:28:44 +0800

    设施

---
+++ Assets/Clock.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    private const float
+        hoursToDegrees = -360f / 12f,
+        minutesToDegrees = -360f / 60f,
+        secondsToDegrees = -360f / 60f;
+
+    public Transform hours, minutes, seconds;
+
+    public bool analog;
+
+    void Update()
+    {
+        if (analog)
+        {
+            TimeSpan timespan = DateTime.Now.TimeOfDay;
+            hours.localRotation =
+                Quaternion.Euler(0f, (float)timespan.TotalHours * -hoursToDegrees, 0f);
+            minutes.localRotation =
+                Quaternion.Euler(0f, (float)timespan.TotalMinutes * -minutesToDegrees, 0f);
+            seconds.localRotation =
+                Quaternion.Euler(0f, (float)timespan.TotalSeconds * -secondsToDegrees, 0f);
+        }
+        else
+        {
+
+            DateTime time = DateTime.Now;
+            hours.localRotation = Quaternion.Euler(0f, time.Hour * -hoursToDegrees, 0f);
+            minutes.localRotation = Quaternion.Euler(0f, time.Minute * -minutesToDegrees, 0f);
+            seconds.localRotation = Quaternion.Euler(0f, time.Second * -secondsToDegrees, 0f);
+        }
+    }
+}
